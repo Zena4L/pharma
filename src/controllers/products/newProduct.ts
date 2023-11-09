ebb463b6ebb8308986de7f3b449a98c76b1db3eb
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-11-09 12:54:23 +0000

    feat

--- src/controllers/products/newProduct.ts
+++ src/controllers/products/newProduct.ts
@@ -2,40 +2,40 @@ import path from "path";
 import { RequestHandler } from "express";
 import Product from "../../models/product";
 
-import multer, { FileFilterCallback } from "multer";
+// import multer, { FileFilterCallback } from "multer";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, "../../public/uploads");
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const extension = file.originalname.split(".").pop();
-    cb(null, "image-" + uniqueSuffix + "." + extension);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     const uploadPath = path.join(__dirname, "../../public/uploads");
+//     cb(null, uploadPath);
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     const extension = file.originalname.split(".").pop();
+//     cb(null, "image-" + uniqueSuffix + "." + extension);
+//   },
+// });
 
-export const uploadDrug = multer({
-  storage: storage,
-  fileFilter: (req, file, cb: FileFilterCallback) => {
-    const filetypes = /jpeg|jpg|png/;
-    const extname = filetypes.test(
-      path.extname(file.originalname).toLowerCase()
-    );
-    const mimetype = filetypes.test(file.mimetype);
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb(new Error("Invalide file"));
-      // cb(null,false);
-    }
-  },
-}).array("image", 10);
+// export const uploadDrug = multer({
+//   storage: storage,
+//   fileFilter: (req, file, cb: FileFilterCallback) => {
+//     const filetypes = /jpeg|jpg|png/;
+//     const extname = filetypes.test(
+//       path.extname(file.originalname).toLowerCase()
+//     );
+//     const mimetype = filetypes.test(file.mimetype);
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     } else {
+//       cb(new Error("Invalide file"));
+//       // cb(null,false);
+//     }
+//   },
+// }).array("image", 10);
 
 export const createProduct: RequestHandler = async (req, res, next) => {
-  const { name, description, price, stockQuantity, category } = req.body;
-  const image = req.files as Express.Multer.File[];
+  const { name, description, price, stockQuantity, category, image } = req.body;
+  // const image = req.files as Express.Multer.File[];
 
   try {
     const newProduct = Product.build({
@@ -44,7 +44,7 @@ export const createProduct: RequestHandler = async (req, res, next) => {
       price,
       stockQuantity,
       category,
-      image: image[0].path,
+      image,
     });
 
     await newProduct.save();
--- src/routes/products/newProduct.ts
+++ src/routes/products/newProduct.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import {
   createProduct,
-  uploadDrug,
+  // uploadDrug,
 } from "../../controllers/products/newProduct";
 import { body } from "express-validator";
 import { requestValidation } from "../../middlewares/requestValidator";
@@ -10,19 +10,19 @@ const router = Router();
 
 router.post(
   "/api/v1/product/new",
-  // [
-  //   body("name").notEmpty().trim().withMessage("Must have a name"),
-  //   body("description")
-  //     .notEmpty()
-  //     .trim()
-  //     .withMessage("Must have a description"),
-  //   body("price").notEmpty().withMessage("Must have a price"),
-  //   body("stockQuantity").notEmpty().withMessage("Must have a stockQuantity"),
-  //   body("category").notEmpty().trim().withMessage("Must have a category"),
-  //   body("image").notEmpty().trim().withMessage("Must have an image"),
-  // ],
-  // requestValidation,
-  uploadDrug,
+  [
+    body("name").notEmpty().trim().withMessage("Must have a name"),
+    body("description")
+      .notEmpty()
+      .trim()
+      .withMessage("Must have a description"),
+    body("price").notEmpty().withMessage("Must have a price"),
+    body("stockQuantity").notEmpty().withMessage("Must have a stockQuantity"),
+    body("category").notEmpty().trim().withMessage("Must have a category"),
+    body("image").notEmpty().trim().withMessage("Must have an image"),
+  ],
+  requestValidation,
+  // uploadDrug,
   createProduct
 );
 
